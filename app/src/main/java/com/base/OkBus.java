4d13334c1f3114c4f20a4864b0ad2d0f814509bb
north2014 <baixiaokang2013@gmail.com>
2016-11-22 15:54:21 +0800

    OkBus优化

--- app/src/main/java/com/base/OkBus.java
+++ app/src/main/java/com/base/OkBus.java
@@ -1,21 +1,20 @@
 package com.base;
 
 import android.os.Message;
+import android.util.SparseArray;
 
 import com.app.aop.utils.LogUtils;
 
 import java.util.ArrayList;
 import java.util.List;
-import java.util.concurrent.ConcurrentHashMap;
 
 /**
  * Created by baixiaokang on 16/11/15.
  */
 
 public class OkBus<T> {
-    private ConcurrentHashMap<Integer, List<Event>> mEventList = new ConcurrentHashMap<>();//存储所有事件ID以及其回调
-    private ConcurrentHashMap<Integer, Object> mStickyEventList = new ConcurrentHashMap<>();//存储粘连事件ID以及其数据
-    private ConcurrentHashMap<Integer, List<Event>> mStickyHasHandedEventList = new ConcurrentHashMap<>();//存储所有粘连事件ID以及已经处理过的回调
+    private volatile SparseArray<List<Event>> mEventList = new SparseArray<>();//存储所有事件ID以及其回调
+    private volatile SparseArray<Object> mStickyEventList = new SparseArray<>();//存储粘连事件ID以及其数据
 
     private OkBus() {
     }
@@ -36,18 +35,13 @@ public class OkBus<T> {
             mList.add(ev);
             mEventList.put(tag, mList);
         }
-        LogUtils.e("Bus register", tag + " :" + mEventList.get(tag).size());
+        LogUtils.e("Bus register", tag + " :");
         if (mStickyEventList.get(tag) != null) {//注册时分发粘连事件
             Message msg = new Message();
             msg.obj = mStickyEventList.get(tag);
             msg.what = tag;
-            List<Event> mStickyHasHandedEvent = mStickyHasHandedEventList.get(tag);//是否有已被处理的回调
-            for (Event mStickyEvent : mEventList.get(tag)) {
-                if (mStickyHasHandedEvent != null && mStickyHasHandedEvent.contains(mStickyEvent)) //已被处理，不再反复触发回调
-                    continue;
-                mStickyEvent.call(msg);
-            }
-            LogUtils.e("mStickyEvent register  and  onEvent", tag + " :" + mEventList.get(tag).size());
+            ev.call(msg);
+            LogUtils.e("mStickyEvent register  and  onEvent", tag + " :");
         }
         return this;
     }
@@ -59,27 +53,19 @@ public class OkBus<T> {
     }
 
     public OkBus onEvent(int tag, T data) {
+        LogUtils.e("Bus onEvent", tag + " ");
         Message msg = new Message();
         msg.obj = data;
         msg.what = tag;
-
-        if (mEventList.get(tag) != null) {
-            LogUtils.e("Bus onEvent", tag + " :" + mEventList.get(tag).size());
+        if (mEventList.get(tag) != null)
             for (Event ev : mEventList.get(tag)) ev.call(msg);
-        }
         return this;
     }
 
     public OkBus onStickyEvent(int tag, T data) {
         LogUtils.e("Bus onStickyEvent", tag + " ");
         mStickyEventList.put(tag, (data == null ? tag : data));
-        Message msg = new Message();
-        msg.obj = data;
-        msg.what = tag;
-        if (mEventList.get(tag) != null) {//触发并加入已处理
-            for (Event ev : mEventList.get(tag)) ev.call(msg);
-            mStickyHasHandedEventList.put(tag, mEventList.get(tag));
-        }
+        onEvent(tag, data);
         return this;
     }
 }
