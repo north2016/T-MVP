638bc29e3be9161fd0ac2b76b43411426626e383
northbai <northbai@lagou.com>
2017-07-13 10:24:36 +0800

    bugfix

--- app/src/main/java/com/base/util/helper/RouterHelper.java
+++ app/src/main/java/com/base/util/helper/RouterHelper.java
@@ -12,9 +12,10 @@ import java.util.HashMap;
  */
 
 public class RouterHelper {
-    
+
     @SingleClick // 防止连续点击
     public static void go(String actionName, HashMap data, View view) {
+        if (data.containsValue(null)) return;
         TRouter.go(actionName, data, view);
     }
 }
